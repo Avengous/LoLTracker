43e1e0364944271099713a8afa223ac1ac74be33
Avengous <xxstryderxx@hotmail.com>
2015-07-24 18:06:50 -0700

    Now will check every x minutes to update a player. Creates players if they do not exist.

--- LoLTracker.py
+++ LoLTracker.py
@@ -1,2 +1,26 @@
+import LoL
+from LoLRequest import Request
+from LoLPlayer import Player
+from LoLDB import Database as DB 
+from time import time,sleep
 
-#- This module interacts with the SQL database.
+CREATE_DATABASE = False
+UPDATE_INTERVAL = 1
+
+if CREATE_DATABASE:
+	DB().createDatabase()
+
+for id in LoL.PLAYERIDS:
+	player = Player(id)
+	if player.recordExists():
+		if (int(time())-player.lastUpdated())/60 >= UPDATE_INTERVAL:
+			print "[UPDATE] {}".format(id)
+			player.update()
+	else:
+		print "[CREATE] {}".format(id)
+		player.create()
+	
+	#Riot API Request Time Interval
+	sleep(15)
+		
+		
